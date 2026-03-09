6bb763aaf4e82577374a6d265a1c2f2d1a8fcc74
ankitdoi--coder <ankitdoi82@gmail.com>
2026-03-09 23:07:14 +0530

    application.properties changed and solved

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO roles (name) VALUES ('PATIENT');
 INSERT INTO roles (name) VALUES ('DOCTOR');
 
 -- Insert default admin (password: Admin9352@healthcare)
-INSERT INTO admins (email, password, name, is_active) VALUES 
-('admin@healthcare.com', '$2a$12$OxDAs7w9iv6Vw81RDR/WvuR/ZzD.3J32BZs5HvVhT5UCzqSLN4RGi', 'System Admin', true);
+-- INSERT INTO admins (email, password, name, is_active) VALUES 
+-- ('admin@healthcare.com', '$2a$12$OxDAs7w9iv6Vw81RDR/WvuR/ZzD.3J32BZs5HvVhT5UCzqSLN4RGi', 'System Admin', true);
